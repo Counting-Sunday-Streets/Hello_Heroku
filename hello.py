6a89fb8e8d51e33f24268e5d340bf181721573f0
Alex Addy <alex@alexaddy.info>
2015-11-10 10:34:26 -0600

    Integrated the timer

--- hello.py
+++ hello.py
@@ -10,8 +10,8 @@ def hello():
 	if request.method == "GET":
 		return render_template('index.html')
 	else:
-		post_to_postgres(request.form['num_people'])
-		return "Success!"
+		post_to_postgres(request.form['buttonValue'])
+		return render_template('index.html')
 
 def post_to_postgres(num_people):
 	conn = psycopg2.connect("dbname=d903vmg658ksh1 user=navthyjfrkmxvh password=1hup-6RLTPuHvpnHpQ9Zte9pcC host=ec2-107-20-223-116.compute-1.amazonaws.com")
