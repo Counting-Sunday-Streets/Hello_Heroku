28877e248655652c3011b6b4ffc5671b932e4dc2
Nicole Mitchell <nem3@rice.edu>
2015-11-12 20:34:59 -0600

    Added the buttons, updated database query

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import os
 import urlparse
 import psycopg2
 from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -10,19 +11,19 @@ def hello():
 	if request.method == "GET":
 		return render_template('index.html')
 	else:
-		post_to_postgres(request.form['buttonValue'])
+		post_to_postgres(request.form['buttonPedValue'], request.form['buttonBikeValue'])
 		return render_template('index.html')
 
 @app.route('/data/')
 def get_data():
 	return str(get_from_postgres());
 
-def post_to_postgres(num_people):
+def post_to_postgres(num_people, num_bikes):
 	conn = psycopg2.connect("dbname=d903vmg658ksh1 user=navthyjfrkmxvh password=1hup-6RLTPuHvpnHpQ9Zte9pcC host=ec2-107-20-223-116.compute-1.amazonaws.com")
 
 	cur = conn.cursor()
 
-	cur.execute("INSERT INTO count VALUES (%s)", (num_people,))
+	cur.execute("INSERT INTO sessions (eid, time, location, count_people, count_bikes) VALUES (%s,%s,%s,%s,%s)", (1, str(datetime.now()), "location", num_people, num_bikes))
 	conn.commit()
 	cur.close()
 	conn.close()
@@ -32,7 +33,7 @@ def get_from_postgres():
 
 	cur = conn.cursor()
 	data = []
-	cur.execute("SELECT * FROM count")
+	cur.execute("SELECT * FROM sessions")
 	
 	for record in cur:
 		data.append(record)
