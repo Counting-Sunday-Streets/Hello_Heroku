cbe47c27e608ce5a75066ae41847c9ae84ef619e
Alex Addy <alex@alexaddy.info>
2015-11-17 10:40:42 -0600

    Switched app to use environment variable instead of hardcoded DB URL

--- hello.py
+++ hello.py
@@ -18,7 +18,8 @@ def get_data():
 	return str(get_from_postgres());
 
 def post_to_postgres(num_people):
-	conn = psycopg2.connect("dbname=d903vmg658ksh1 user=navthyjfrkmxvh password=1hup-6RLTPuHvpnHpQ9Zte9pcC host=ec2-107-20-223-116.compute-1.amazonaws.com")
+	#conn = psycopg2.connect("dbname=d903vmg658ksh1 user=navthyjfrkmxvh password=1hup-6RLTPuHvpnHpQ9Zte9pcC host=ec2-107-20-223-116.compute-1.amazonaws.com")
+	conn = connect_postgres()
 
 	cur = conn.cursor()
 
@@ -28,7 +29,7 @@ def post_to_postgres(num_people):
 	conn.close()
 
 def get_from_postgres():
-	conn = psycopg2.connect("dbname=d903vmg658ksh1 user=navthyjfrkmxvh password=1hup-6RLTPuHvpnHpQ9Zte9pcC host=ec2-107-20-223-116.compute-1.amazonaws.com")
+	conn = connect_postgres()
 
 	cur = conn.cursor()
 	data = []
@@ -40,6 +41,18 @@ def get_from_postgres():
 	cur.close()
 	conn.close()
 	return data
+
+def connect_postgres():
+	urlparse.uses_netloc.append("postgres")
+	url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+	conn = psycopg2.connect(
+    	database=url.path[1:],
+    	user=url.username,
+    	password=url.password,
+    	host=url.hostname,
+    	port=url.port)
+	return conn
 
 if __name__ == '__main__':
 	app.debug = True
---
+++ static/js/timer.js
@@ -0,0 +1,60 @@
+if ('addEventListener' in document) {
+        document.addEventListener('DOMContentLoaded', function() {
+          FastClick.attach(document.body);
+        }, false);
+      }
+      var count = 0;
+      var button = document.getElementById("countButton");
+      var start = document.getElementById("startButton");
+      var displayNum = document.getElementById("displayCount");
+      var buttonValue = document.getElementById("buttonValue")
+
+      start.onclick = function(){
+        display = document.querySelector('#time');
+        startTimer(5, display);
+      }
+
+      button.onclick = function(){
+        count++;
+        displayNum.innerHTML = count;
+      }
+
+      function startTimer(duration, display) {
+        var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+        function timer() {
+        // get the number of seconds that have elapsed since 
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds; 
+
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
+          }
+        if (diff == 0) {
+          buttonValue.value = count;
+          document.getElementById("counter").submit();
+        }
+        };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+  }
+
+  //window.onload = function () {
+    //var fiveMinutes = 60 * 15,
+    //display = document.querySelector('#time');
+    //startTimer(10, display);
+  //};
