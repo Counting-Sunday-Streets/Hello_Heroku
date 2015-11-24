2d6fefeaa1406efdc4af10924eda3f696c5001ec
Alex Addy <alex@alexaddy.info>
2015-11-23 22:00:16 -0600

    Data now does integration

--- hello.py
+++ hello.py
@@ -4,9 +4,14 @@ import psycopg2
 from flask import Flask, render_template, request
 import calendar
 import time
+import collections
+import scipy.integrate
 
 app = Flask(__name__)
 
+###
+###  PAGES
+###
 @app.route('/', methods=['GET', 'POST'])
 def hello():
 	if request.method == "GET":
@@ -32,7 +37,18 @@ def select_event():
 
 @app.route('/data/')
 def get_data():
-	return str(get_from_postgres());
+	totals = run_stats()
+	PAGE = ""
+
+	for location, total in totals.items():
+		PAGE += location 
+		PAGE += " - " 
+		PAGE += "People: " 
+		PAGE += str(total[0])
+		PAGE += ", Bikes: " 
+		PAGE += str(total[1])
+		PAGE += "\n"
+	return PAGE
 
 def post_to_postgres(num_people, num_bikes):
 	conn = connect_postgres()
@@ -41,22 +57,7 @@ def post_to_postgres(num_people, num_bikes):
 
 	cur.execute("INSERT INTO sessions (eid, time, location, count_people, count_bikes) VALUES (%s,%s,%s,%s,%s)", (1, calendar.timegm(time.gmtime()), "location", num_people, num_bikes))
 	conn.commit()
-	cur.close()
-	conn.close()
-
-def get_from_postgres():
-	conn = connect_postgres()
-
-	cur = conn.cursor()
-	data = []
-	cur.execute("SELECT * FROM sessions")
-	
-	for record in cur:
-		data.append(record)
-
-	cur.close()
-	conn.close()
-	return data
+	end_postgres(conn, cur)
 
 @app.route('/displaydata/', methods=['GET', 'POST'])
 def display_data():
@@ -74,7 +75,71 @@ def display_data():
 	else:
 		#post_to_postgres(request.form['buttonPedValue'], request.form['buttonBikeValue'])
 		return render_template('display_data.html', events=events,data=dict(eid=0, nump=0, numb=0))
+	end_postgres(conn, cur)
 
+###
+### STATS
+###
+
+INTERVAL = int(os.environ["INTERVAL"])
+START_TIME = int(os.environ["START_TIME"])
+
+def integrate_simps(points):
+	"""
+	Takes a list of tuples and integrates over them
+	"""
+	y_val = [tup[1] for tup in points]
+	x_val = [tup[0] for tup in points]
+
+	if START_TIME not in x_val:
+		x_val.insert(0, START_TIME)
+		y_val.insert(0, 0)
+
+	return scipy.integrate.simps(y_val, x_val, 'avg')
+
+def pull_results(conn):
+	"""
+	Queries the database and returns lists of tuple rows mapped to locations
+	"""
+	cur = conn.cursor()
+
+	cur.execute("SELECT time,location,count_people,count_bikes FROM sessions;")
+	
+	data_people = collections.defaultdict(list)
+	data_bikes = collections.defaultdict(list)
+	for row in cur.fetchall():
+		people = []
+		bikes = []
+
+		start_time = row[0] - INTERVAL
+		rate_people = float(row[2]) / INTERVAL
+		rate_bikes = float(row[3]) / INTERVAL
+
+		people.extend([(start_time, rate_people),(row[0], rate_people)])
+		bikes.extend([(start_time, rate_bikes), (row[0], rate_bikes)])
+
+		data_people[row[1]].extend(people)
+		data_bikes[row[1]].extend(bikes)
+
+	end_postgres(conn, cur)
+	return data_people, data_bikes
+
+def run_stats():
+	conn = connect_postgres()
+	data_people, data_bikes = pull_results(conn)
+	totals = collections.defaultdict(list)
+
+	for location, data in data_people.items():
+		totals[location].append(integrate_simps(data))
+
+	for location, data in data_bikes.items():
+		totals[location].append(integrate_simps(data))
+
+	return totals
+
+###
+### DATABASE CONNECTION MAINTAINANCE
+###
 
 def connect_postgres():
 	urlparse.uses_netloc.append("postgres")
@@ -87,6 +152,10 @@ def connect_postgres():
     	host=url.hostname,
     	port=url.port)
 	return conn
+
+def end_postgres(conn, cur):
+	cur.close()
+	conn.close()
 
 if __name__ == '__main__':
 	app.debug = True
